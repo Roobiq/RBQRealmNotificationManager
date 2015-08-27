2dd14413b36e9014e139d46cc62a5035f81d57fe
Adam Fish <af@realm.io>
2015-08-27 13:03:27 -0700

    Fix to make sure the object is added first before trying to create a RBQSafeRealmObject.

--- Helpers/RLMRealm+Notifications.m
+++ Helpers/RLMRealm+Notifications.m
@@ -14,9 +14,9 @@
 
 - (void)addObjectWithNotification:(RLMObject *)object
 {
-    [[RBQRealmChangeLogger loggerForRealm:self] didAddObject:object];
-    
     [self addObject:object];
+    
+    [[RBQRealmChangeLogger loggerForRealm:self] didAddObject:object];
 }
 
 - (void)addObjectsWithNotification:(id<NSFastEnumeration>)array
@@ -33,14 +33,14 @@
 
 - (void)addOrUpdateObjectWithNotification:(RLMObject *)object
 {
+    [self addOrUpdateObject:object];
+    
     if (object.realm != self && ![object isContainedInRealm:self]) {
         [[RBQRealmChangeLogger loggerForRealm:self] didAddObject:object];
     }
     else {
         [[RBQRealmChangeLogger loggerForRealm:self] didChangeObject:object];
     }
-    
-    [self addOrUpdateObject:object];
 }
 
 - (void)addOrUpdateObjectsFromArrayWithNotification:(id<NSFastEnumeration>)array
